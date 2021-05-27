83eab0bfe7bfe6f8fbaa5d7cb9a3f5cc588f8214
DeliPl <delicat.kuba@gmail.com>
2021-05-27 21:37:06 +0200

    Init cichyEvent

---
+++ Discord bot/cichyEvent.py
@@ -0,0 +1,12 @@
+class CichyEvent:
+    
+
+
+class SingleQuiz:
+    quastion = str()
+    answers = []
+
+    def __init__(sefl, question, answers):
+        self.answers = answers
+        quastion = question
+
